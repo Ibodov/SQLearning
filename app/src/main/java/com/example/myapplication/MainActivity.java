ddf4220d147e26f1d58d8563658df83b3cc34093
Android3 <billing@babilon-t.com>
2021-04-02 13:48:48 +0500

    Добавил метод добавление в БД и удаления из БД

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
@@ -23,8 +27,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        header = (TextView)findViewById(R.id.header);
+//        header = (TextView)findViewById(R.id.header);
         userList = (ListView)findViewById(R.id.list);
+        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+//Другую ситуацию представляет обработчик нажатия на элемент списка - при нажатии также будет
+// запускаться UserActivity, но теперь будет передаваться id выбранной записи:
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+                intent.putExtra("id", id);
+                startActivity(intent);
+            }
+        });
 
         databaseHelper = new DatabaseHelper(getApplicationContext());
     }
@@ -41,9 +55,18 @@ public class MainActivity extends AppCompatActivity {
         // создаем адаптер, передаем в него курсор
         userAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item,
                 userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
-        header.setText("Найдено элементов: " +  userCursor.getCount());
+//        header.setText("Найдено элементов: " +  userCursor.getCount());
         userList.setAdapter(userAdapter);
     }
+
+//При нажатии на кнопку запускается UserActivity, при этом не передается никакого id,
+// то есть в UserActivity id будет равен нулю, значит будет идти добавление данных:
+
+    // по нажатию на кнопку запускаем UserActivity для добавления данных
+public void add(View view) {
+        Intent intent = new Intent(this, UserActivity.class);
+        startActivity(intent);
+}
 
     @Override
     public void onDestroy(){
--- app/src/main/java/com/example/myapplication/UserActivity.java
+++ app/src/main/java/com/example/myapplication/UserActivity.java
@@ -35,11 +35,15 @@ public class UserActivity extends AppCompatActivity {
         sqlHelper = new DatabaseHelper(this);
         db = sqlHelper.getWritableDatabase();
 
+//При обновлении или удалении объекта из списка из главной activity в UserActivity будет передаваться id объекта:
+// long userId=0;
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             userId = extras.getLong("id");
         }
+//Если из MainActivity не было передано id, то устанавливаем его значение 0, следовательно, у нас будет добавление, а не редактирование/удаление
         // если 0, то добавление
+//Если id определен, то получаем по нему из базы данных объект для редактирования/удаления:
         if (userId > 0) {
             // получаем элемент по id из бд
             userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE + " where " +
@@ -55,21 +59,48 @@ public class UserActivity extends AppCompatActivity {
     }
 
     public void save(View view){
+//Для добавления или обновления нам надо создать объект ContentValues. Данный объект представляет словарь, который содержит
+// набор пар "ключ-значение". Для добавления в этот словарь нового объекта применяется метод put. Первый параметр
+// метода - это ключ, а второй - значение, например:
+
+//В данном же случае добавляются введенные в текстовое поля значения:
         ContentValues cv = new ContentValues();
         cv.put(DatabaseHelper.COLUMN_NAME, nameBox.getText().toString());
         cv.put(DatabaseHelper.COLUMN_YEAR, Integer.parseInt(yearBox.getText().toString()));
 
         if (userId > 0) {
+//При обновлении в метод update() передается название таблицы, объект ContentValues
+// и критерий, по которому происходит обновление (в данном случае столбец id):
             db.update(DatabaseHelper.TABLE, cv, DatabaseHelper.COLUMN_ID + "=" + String.valueOf(userId), null);
         } else {
+//Метод insert() принимает название таблицы, объект ContentValues с добавляемыми значениями.
+// Второй параметр является необязательным: он передает столбец, в который надо добавить значение NULL:
             db.insert(DatabaseHelper.TABLE, null, cv);
         }
+//Вместо этих методов, как в прошлой теме, можно использовать метод execSQL() с точным указанием выполняемого sql-выражения.
+// В то же время методы delete/insert/update имеют преимущество - они возвращают id измененной записи,
+// по которому мы можем узнать об успешности операции, или -1 в случае неудачной операции:
+ /*       long result = db.insert(DatabaseHelper.TABLE, null, cv);
+        if(result>0){
+
+            // действия
+        }
+*/
+        //После каждой операции выполняется метод goHome(), который возвращает на главную activity.
         goHome();
     }
+
+//Для выполнения операций по вставке, обновлению и удалению данных SQLiteDatabase имеет методы insert(), update() и delete().
+// Эти методы вызываются в обработчиках кнопок:
     public void delete(View view){
         db.delete(DatabaseHelper.TABLE, "_id = ?", new String[]{String.valueOf(userId)});
         goHome();
     }
+// В метод delete() передается название таблицы, а также столбец, по которому происходит удаление, и его значение. В качестве критерия можно
+// выбрать несколько столбцов, поэтому третьим параметром идет массив. Знак вопроса ? обозначает параметр, вместо которого подставляется
+// значение из третьего параметра.
+
+
     private void goHome(){
         // закрываем подключение
         db.close();
